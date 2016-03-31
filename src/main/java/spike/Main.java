33befa3a86e1aa7107bb650c4536b2d1b078f563
Matias Waterloo <matiwater@gmail.com>
2016-03-31 13:57:05 -0500

    2

---
+++ src/main/java/spike/Main.java
@@ -0,0 +1,11 @@
+package spike;
+
+public class Main{
+
+public static void main(String [ ] args)
+{
+      System.out.println("HW!");
+}
+
+
+}
---
+++ src/test/java/spike/TestMain.java
@@ -0,0 +1,13 @@
+package spike;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class TestMain{
+
+@Test public void test(){
+	assertTrue(true);
+}
+
+}
