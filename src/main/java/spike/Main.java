cf35b66b61a2db7114d5cf36f2b1dc9df69b3cba
Matias Waterloo <matiwater@gmail.com>
2016-08-12 11:26:55 -0500

    +5

--- src/main/java/spike/Main.java
+++ src/main/java/spike/Main.java
@@ -4,7 +4,6 @@ public class Main{
 
 	public static void main(String [ ] args){
       	System.out.println("Hello!");
-      	Thread.sleep(10*60*1000);
       	System.out.println("World!");
 	}
 
--- src/test/java/spike/TestMain.java
+++ src/test/java/spike/TestMain.java
@@ -6,10 +6,11 @@ import static org.junit.Assert.*;
 
 public class TestMain{
 
-@Test public void test(){
+@Test public void test()throws InterruptedException{
 	assertTrue(3+4==7);
 	assertTrue(2+2==4);
 	assertTrue(5+4+1+2==12);
+	      	Thread.sleep(10*60*1000);
 }
 
 }
